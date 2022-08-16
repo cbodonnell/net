9a083391f40d0f7a8cafd6d6454a22640eab276f
Craig O'Donnell <cbodonnell@comcast.net>
2022-08-15 23:02:54 -0400

    udp registration

--- examples/udp/client/main.go
+++ examples/udp/client/main.go
@@ -7,44 +7,101 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("Please provide a host:port string")
-		return
+	if len(os.Args) < 2 {
+		log.Fatalln("Please provide a signal server address")
 	}
-	CONNECT := os.Args[1]
+	signalAddr := os.Args[1]
 
-	s, err := net.ResolveUDPAddr("udp4", CONNECT)
-	c, err := net.DialUDP("udp4", nil, s)
+	if len(os.Args) < 3 {
+		log.Fatalln("Please provide a server name to punch to")
+	}
+	serverName := os.Args[2]
+
+	signal, err := net.ResolveUDPAddr("udp4", signalAddr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to resolve signal address: %s", err)
 	}
 
-	fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
-	defer c.Close()
+	listen, _ := net.ListenUDP("udp", nil)
+	defer listen.Close()
+
+	fmt.Printf("Listening on %s\n", listen.LocalAddr().String())
+
+	target, err := punch(listen, signal, serverName)
+	if err != nil {
+		log.Fatalf("Failed to punch: %s", err)
+	}
+
+	fmt.Printf("Punched to %s\n", target.String())
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print(">> ")
-		text, _ := reader.ReadString('\n')
-		data := []byte(text + "\n")
-		_, err = c.Write(data)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalln("Failed to read from stdin: ", err)
+		}
+		text = strings.TrimSpace(text)
+		data := []byte(text)
+
+		listen.SetDeadline(time.Now().Add(time.Second * 5))
+		_, err = listen.WriteTo(data, target)
 		if strings.TrimSpace(string(data)) == "STOP" {
 			fmt.Println("Exiting UDP client!")
 			return
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
+			return
 		}
 
 		buffer := make([]byte, 1024)
-		n, _, err := c.ReadFromUDP(buffer)
+		n, remoteAddr, err := listen.ReadFromUDP(buffer)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
+			return
 		}
-		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+		fmt.Printf("Reply from %s:\n%s\n", remoteAddr.String(), string(buffer[:n]))
+	}
+}
+
+func punch(listen *net.UDPConn, signal *net.UDPAddr, name string) (*net.UDPAddr, error) {
+	fmt.Printf("Punching to client %s on signal server %s\n", name, signal.String())
+
+	listen.SetDeadline(time.Now().Add(time.Second * 5))
+	defer listen.SetDeadline(time.Time{})
+
+	_, err := listen.WriteTo([]byte(fmt.Sprintf("PUNCH: %s", name)), signal)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := make([]byte, 1024)
+	n, _, err := listen.ReadFromUDP(buffer)
+	if err != nil {
+		return nil, err
+	}
+	response := buffer[0:n]
+
+	parts := strings.Split(string(response), ": ")
+	if len(parts) != 2 {
+		return nil, err
+	}
+
+	result, value := parts[0], parts[1]
+
+	switch result {
+	case "SUCCESS":
+		target, _ := net.ResolveUDPAddr("udp4", value)
+		return target, nil
+	case "FAIL":
+		return nil, fmt.Errorf("failed to punch to %s: %s", name, value)
+	default:
+		return nil, fmt.Errorf("unknown result: %s", result)
 	}
 }
--- examples/udp/server/main.go
+++ examples/udp/server/main.go
@@ -3,53 +3,119 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
 
-func random(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
 func main() {
-	if len(os.Args) == 1 {
-		log.Fatalln("Please provide a port number!")
+	if len(os.Args) < 2 {
+		log.Fatalln("Please provide a signal server address")
 		return
 	}
-	PORT := ":" + os.Args[1]
+	signalAddr := os.Args[1]
 
-	s, err := net.ResolveUDPAddr("udp4", PORT)
+	if len(os.Args) < 3 {
+		log.Fatalln("Please provide a name")
+		return
+	}
+	name := os.Args[2]
+
+	remote, err := net.ResolveUDPAddr("udp", signalAddr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to resolve remote address: %s", err)
+	}
+	// local, err := net.ResolveUDPAddr("udp", localAddr)
+	// if err != nil {
+	// 	log.Fatalf("Failed to resolve local address: %s", err)
+	// }
+	listen, err := net.ListenUDP("udp", nil)
+	if err != nil {
+		log.Fatalf("Failed to listen: %s", err)
+	}
+	defer listen.Close()
+
+	if err = register(listen, remote, name); err != nil {
+		log.Fatalf("Failed to register: %s", err)
 	}
 
-	connection, err := net.ListenUDP("udp4", s)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		for {
+			if err = ping(listen, remote, name); err != nil {
+				log.Fatalf("Failed to ping: %s", err)
+			}
+			time.Sleep(time.Second * 5)
+		}
+	}()
 
-	defer connection.Close()
-	buffer := make([]byte, 1024)
-	rand.Seed(time.Now().Unix())
+	fmt.Printf("Listening on %s\n", listen.LocalAddr().String())
 
 	for {
-		n, addr, err := connection.ReadFromUDP(buffer)
-		fmt.Print("-> ", string(buffer[0:n-1]))
-
-		if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
-			fmt.Println("Exiting UDP server!")
-			return
-		}
-
-		data := []byte(strconv.Itoa(random(1, 1001)))
-		fmt.Printf("data: %s\n", string(data))
-		_, err = connection.WriteToUDP(data, addr)
+		buffer := make([]byte, 1024)
+		bytesRead, remoteAddr, err := listen.ReadFromUDP(buffer)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("[ERROR] Failed to read from UDP: %s\n", err.Error())
+			continue
+		}
+		message := buffer[:bytesRead]
+
+		fmt.Printf("[INCOMING] from %s:\n%s\n", remoteAddr.String(), string(message))
+
+		_, err = listen.WriteToUDP(message, remoteAddr)
+		if err != nil {
+			fmt.Printf("[ERROR] Failed to write to %s: %s\n", remoteAddr.String(), err.Error())
+			continue
 		}
 	}
+}
+
+func register(listen *net.UDPConn, remoteAddr *net.UDPAddr, name string) error {
+	fmt.Printf("Registering with signal server %s\n", remoteAddr.String())
+
+	listen.SetDeadline(time.Now().Add(time.Second * 5))
+	defer listen.SetDeadline(time.Time{})
+
+	_, err := listen.WriteTo([]byte(fmt.Sprintf("REGISTER: %s", name)), remoteAddr)
+	if err != nil {
+		return fmt.Errorf("failed to write to signal server %s: %s", remoteAddr.String(), err.Error())
+	}
+
+	buffer := make([]byte, 1024)
+	n, _, err := listen.ReadFromUDP(buffer)
+	if err != nil {
+		return fmt.Errorf("failed to read from signal server %s: %s", remoteAddr.String(), err.Error())
+	}
+
+	response := string(buffer[:n])
+
+	parts := strings.Split(response, ": ")
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid response from signal server %s: %s", remoteAddr.String(), response)
+	}
+	action, target := parts[0], parts[1]
+
+	switch action {
+	case "SUCCESS":
+		fmt.Printf("Registered as %s\n", target)
+		return nil
+	case "FAIL":
+		return fmt.Errorf("failed to register as %s: %s", name, target)
+	default:
+		return fmt.Errorf("unknown action: %s", action)
+	}
+}
+
+func ping(listen *net.UDPConn, remoteAddr *net.UDPAddr, name string) error {
+	listen.SetDeadline(time.Now().Add(time.Second * 5))
+	defer listen.SetDeadline(time.Time{})
+
+	_, err := listen.WriteTo([]byte(fmt.Sprintf("PING: %s", name)), remoteAddr)
+	if err != nil {
+		return fmt.Errorf("failed to write to signal server %s: %s", remoteAddr.String(), err.Error())
+	}
+	// TODO: Need to read something from here to make sure the ping was successful
+	// Maybe can add a switch statement on thew remoteAddr.String() to handle different senders
+
+	return nil
 }
